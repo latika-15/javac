6829597712434ae21a8935da1e0a57b503d69d0f
latika-15 <lavitewari15@gamil.com>
2024-05-11 17:40:29 +0530

    triangle basic sout

---
+++ patterns/ldrighttri.java
@@ -0,0 +1,12 @@
+import java.lang.* ;
+
+public class ldrighttri {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+    
+}
+
