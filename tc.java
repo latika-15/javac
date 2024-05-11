75e266b55a081d188f431ffc5b04a32c077325fc
latika-15 <lavitewari15@gamil.com>
2024-05-11 17:41:45 +0530

    all were added

---
+++ tc.java
@@ -0,0 +1,18 @@
+public class tc{
+    public static void main(String[] args) {
+
+        //implicit
+        int a=24;
+        float b=a;
+        System.out.println("IMPLICIT");
+        System.out.println(a);
+        System.out.println(b);
+
+        //explicit
+        double pi=3.14159;;
+        int rev=(int) pi;
+        System.out.println("EXPLICIT");
+        System.out.println(pi);
+        System.out.println(rev);
+    }
+}
