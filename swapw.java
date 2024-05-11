6e2201b6ffac404f052b65c3c6a54144a0ce284c
latika-15 <lavitewari15@gamil.com>
2024-05-11 17:39:08 +0530

    swapping of two numbers without using third variable

---
+++ swapw.java
@@ -0,0 +1,12 @@
+public class swapw {
+    public static void main(String[] args) {
+        int a=14;
+        int b=22;
+        System.out.println("before swapping: a="+a+" b="+b);
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("after swapping: a="+a+" b="+b);
+    }
+    
+}
