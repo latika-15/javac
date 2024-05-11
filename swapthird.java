18db8a39ad32b058a28ac790073418f24c788bec
latika-15 <lavitewari15@gamil.com>
2024-05-11 17:38:35 +0530

    swapping of two numbers using third variable

---
+++ swapthird.java
@@ -0,0 +1,13 @@
+public class swapthird {
+    public static void main(String[] args) {
+        int a=10;
+        int b=15;
+        int temp;
+        System.out.println("before swapping: a="+a+" b="+b);
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("after swapping: a="+a+" b="+b);
+
+    }
+}
